401c25cfefc08d1c66649d10674a2404fa7b8247
jasonrla <jasonrla@gmail.com>
2024-04-11 14:19:41 -0500

    update yml

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,10 @@ package tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.AndroidServerFlag;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -16,7 +20,7 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static void Android_setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "11"); //version del android
+        capabilities.setCapability("platformVersion", "14"); //version del android
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("automationName", "uiautomator2");
         capabilities.setCapability("app",
@@ -32,7 +36,7 @@ public class TestBase extends AbstractTestNGCucumberTests {
         capabilities.setCapability("isHeadless",true);
         capabilities.setCapability("app",
                 System.getProperty("user.dir") + "/apps/DailyCheck.zip");
-        driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), capabilities);
+        driver = new IOSDriver<>(new URL("http://localhost:4723/"), capabilities);
     }
 
     public static void tearDown() {
