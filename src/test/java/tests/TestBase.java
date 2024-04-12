cb490a33828cfa6c79036a74098567afd2a5a315
jasonrla <jasonrla@gmail.com>
2024-04-12 08:26:54 -0500

    api level 23 v6

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,7 +16,7 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static void Android_setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "11"); //10 en CI
+        capabilities.setCapability("platformVersion", "6"); //10 en CI
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("automationName", "uiautomator2");
         capabilities.setCapability("app",
