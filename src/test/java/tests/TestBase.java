5f6f4e19dd6d1a672ca48d04ad9252c0732d64b8
jasonrla <jasonrla@gmail.com>
2024-04-11 22:25:16 -0500

    api level 33 v13

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,10 +3,6 @@ package tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import io.appium.java_client.service.local.flags.AndroidServerFlag;
-import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -20,7 +16,7 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static void Android_setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "10"); //10 en CI
+        capabilities.setCapability("platformVersion", "13"); //10 en CI
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("automationName", "uiautomator2");
         capabilities.setCapability("app",
