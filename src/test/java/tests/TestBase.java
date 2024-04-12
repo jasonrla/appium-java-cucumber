aa1b68e9fb0c61560df8a7b37c7b845ab2dc68c9
jasonrla <jasonrla@gmail.com>
2024-04-12 11:31:04 -0500

    improvement

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,12 +14,11 @@ public class TestBase {
 
     public static void Android_setUp() throws MalformedURLException {
 
+        String apiLevel = System.getProperty("api.level");
+        System.out.printf("API Level: %s\n", apiLevel);
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        //capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        //capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator");
-
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "9"); //10 en CI
+        capabilities.setCapability("platformVersion", getPlatformVersionFromApiLevel(apiLevel)); //28:9 , 29:10 en CI
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("automationName", "uiautomator2");
         capabilities.setCapability("app",
@@ -36,6 +35,43 @@ public class TestBase {
         capabilities.setCapability("app",
                 System.getProperty("user.dir") + "/apps/DailyCheck.zip");
         driver = new IOSDriver(new URL("http://localhost:4723/"), capabilities);
+    }
+
+    public static String getPlatformVersionFromApiLevel(String apiLevel) {
+        switch (apiLevel) {
+            case "21":
+                return "5.0";
+            case "22":
+                return "5.1";
+            case "23":
+                return "6.0";
+            case "24":
+                return "7.0";
+            case "25":
+                return "7.1";
+            case "26":
+                return "8.0";
+            case "27":
+                return "8.1";
+            case "28":
+                return "9.0";
+            case "29":
+                return "10.0";
+            case "30":
+                return "11.0";
+            case "31":
+                return "12.0";
+            case "32":
+                return "12.0";
+            case "33":
+                return "13.0";
+            case "34":
+                return "14.0";
+            case "35":
+                return "15.0";
+            default:
+                throw new IllegalArgumentException("API Level " + apiLevel + " is not supported");
+        }
     }
 
     public static void tearDown() {
