9cf5c0e9760debf5f59189e6329af8bd8c97b3b3
jasonrla <jasonrla@gmail.com>
2024-04-12 08:47:10 -0500

    api level 29 v10

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,9 +14,13 @@ public class TestBase extends AbstractTestNGCucumberTests {
     public static AppiumDriver driver;
 
     public static void Android_setUp() throws MalformedURLException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        //capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        //capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator");
+
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "6"); //10 en CI
+        capabilities.setCapability("platformVersion", "10"); //10 en CI
         capabilities.setCapability("deviceName", "Android Emulator");
         capabilities.setCapability("automationName", "uiautomator2");
         capabilities.setCapability("app",
